00bda3bdd2031647b6091801c06afa84c9e04f0e
Kaesi Manakkal <kaesimanakkal@gmail.com>
2022-04-14 15:01:21 -0400

    add backtrack solver

--- src/Solver.java
+++ src/Solver.java
@@ -3,11 +3,11 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Solver {
-    int gridSize = 9;               // default sudoku size: 9
-    int grid[][];                   // our 2d matrix "sudoku board"
+    int boardSize = 9;               // default sudoku size: 9
+    int board[][];                   // our 2d matrix "sudoku board"
 
     Solver() {
-        grid = new int[gridSize][gridSize];
+        board = new int[boardSize][boardSize];
     }
 
     // read in puzzle
@@ -17,17 +17,17 @@ public class Solver {
             File puzzleFile = new File(filename);
             Scanner puzzleReader = new Scanner(puzzleFile);
 
-            // get grid size for problem
-            /*String gridSizeStr = puzzleReader.nextLine();
-            gridSize = Integer.parseInt(gridSizeStr);
-            grid = new int[gridSize][gridSize]; */
+            // get board size for problem
+            /*String boardSizeStr = puzzleReader.nextLine();
+            boardSize = Integer.parseInt(boardSizeStr);
+            board = new int[boardSize][boardSize]; */
 
-            // read in grid[][]
+            // read in board[][]
             while(puzzleReader.hasNextLine()) {
                 String data = puzzleReader.nextLine();
                 String[] tokenizedRow = data.split(" ");
-                for(int j=0; j<gridSize; j++) {
-                    grid[currentRow][j] = Integer.parseInt(tokenizedRow[j]);
+                for(int j=0; j<boardSize; j++) {
+                    board[currentRow][j] = Integer.parseInt(tokenizedRow[j]);
                 }
                 currentRow++;
             }
@@ -37,10 +37,10 @@ public class Solver {
         }
     }
 
-    void printGrid() {
-        for(int i=0; i<gridSize; i++) {
-            for(int j=0; j<gridSize; j++) {
-                System.out.print(grid[i][j] + " ");
+    void printBoard() {
+        for(int i=0; i<boardSize; i++) {
+            for(int j=0; j<boardSize; j++) {
+                System.out.print(board[i][j] + " ");
             }
             System.out.println();
         }
@@ -49,20 +49,20 @@ public class Solver {
     boolean usedInBox(int startRow, int startCol, int number) {
         for (int row = 0; row < 3; row++)
             for (int col = 0; col < 3; col++)
-                if (grid[row + startRow][col + startCol] == number)
+                if (board[row + startRow][col + startCol] == number)
                     return true;
         return false;
     }
 
     boolean isLegal(int row, int col, int number) {
         // check row
-        for(int i=0; i<gridSize; i++)
-            if(grid[row][i] == number)
+        for(int i=0; i<boardSize; i++)
+            if(board[row][i] == number)
                 return false;
     
         // check col
-        for(int i=0; i<gridSize; i++)
-            if(grid[i][col] == number)
+        for(int i=0; i<boardSize; i++)
+            if(board[i][col] == number)
                 return false;
 
         // check if we used this number in this box already
@@ -75,23 +75,23 @@ public class Solver {
 
     boolean naiveSolver(int row, int col) {
         // check if we have reached the end
-        if((row == gridSize-1) && (col == gridSize))
+        if((row == boardSize-1) && (col == boardSize))
             return true;
         
         // if we reach end of a col, move to next row
-        if(col == gridSize) {
+        if(col == boardSize) {
             row++;
             col = 0;
         }
 
         // current pos already has an item
-        if(grid[row][col] > 0)
+        if(board[row][col] > 0)
             return naiveSolver(row, col+1);
         
-        for(int number = 1; number <= gridSize; number++) {
+        for(int number = 1; number <= boardSize; number++) {
             if(isLegal(row, col, number)) {
                 // use number in current row
-                grid[row][col] = number;
+                board[row][col] = number;
 
                 // continue solving
                 if(naiveSolver(row, col+1))
@@ -99,10 +99,53 @@ public class Solver {
             }
 
             // the number did not work out, try again
-            grid[row][col] = 0;
+            board[row][col] = 0;
         }
 
         // none of the numbers worked, no solution possible
         return false;
     }
+
+    /*boolean findEmpty(int row, int col) {
+        for(row = 0; row < boardSize; row++)
+            for(col = 0; col < boardSize; col++)
+                if(board[row][col] == 0)
+                    return true;
+        return false;
+    }*/
+
+    boolean backtrackSolver() {
+        int row = 0, col = 0;
+        boolean done = true;
+        // check if we have reached the end
+        for(int i = 0; i < boardSize; i++) {
+            for(int j = 0; j < boardSize; j++) {
+                if(board[i][j] == 0) {
+                    row=i; col=j;
+                    done = false;
+                    break;
+                }
+            }
+            if(!done)
+                break;
+        }
+
+        if(done)
+            return true;
+        
+        for(int number=1; number<=9; number++) {
+            if(isLegal(row, col, number)) {
+                board[row][col] = number;
+
+                // recursive call
+                if(backtrackSolver())
+                    return true;
+                
+                board[row][col] = 0;
+            }
+        }
+
+        return false;
+    }
+
 }
--- src/Sudoku.java
+++ src/Sudoku.java
@@ -2,17 +2,35 @@ public class Sudoku {
 
     public static void main(String[] args) {
         Solver sudoku = new Solver();
-        if(args.length < 1) {
-            System.out.println("Usage: solver <puzzle file>");
+        boolean solved = false;
+        if(args.length < 2) {
+            System.out.println("Usage: solver <puzzle file> <mode>");
             System.exit(1);
         }
 
         // load input
         sudoku.loadPuzzle(args[0]);
 
-        sudoku.naiveSolver(0, 0);
-        // TODO: call solver here
+        long start = System.currentTimeMillis();
+
+        switch(args[1]) {
+            case "0":
+                solved = sudoku.naiveSolver(0, 0);
+                break;
+            case "1":
+                solved = sudoku.backtrackSolver();
+                break;
+            default:
+                System.out.println("valid modes: 0, 1");
+        }
+        long end = System.currentTimeMillis();
+
+        float timeTaken = (end-start)/1000f;
+
         // print output
-        sudoku.printGrid();
+        System.out.println("==========================");
+        System.out.format("Algo: %s; Time: %05f\n", args[1], timeTaken);
+        System.out.println("Solution: ================");
+        if(solved) sudoku.printBoard();
     }
 }
