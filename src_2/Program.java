a7a89e6f51e1648f239df5ee03334da6e2d14b24
Kaesi Manakkal <kaesimanakkal@gmail.com>
2022-05-04 14:58:10 -0400

    add noprint option

--- src_2/Program.java
+++ src_2/Program.java
@@ -1,8 +1,10 @@
 public class Program {
     
     public static void main(String[] args) {
-        if (args.length != 2) {
-            System.err.println("[USAGE]: ./run.sh <sudoku file> <0|1|2>");
+        boolean print = true;
+
+        if (args.length < 2) {
+            System.err.println("[USAGE]: ./run.sh <sudoku file> <0|1|2> [noprint]");
             System.exit(-1);
         }
 
@@ -14,7 +16,7 @@ public class Program {
         try {
             choice = Integer.parseInt(args[1]); 
         } catch (Exception e) {
-            System.err.println("[USAGE]: ./run.sh <sudoku file> <0|1|2>");
+            System.err.println("[USAGE]: ./run.sh <sudoku file> <0|1|2> [noprint]");
             System.exit(-1);
         }
         ISolver solver;
@@ -28,13 +30,22 @@ public class Program {
             default: throw new IllegalArgumentException("Invalid solver.");
         }
         
-        s.printBoard();
-        System.out.println("-----------------");
+        if(args.length >= 3)
+            if(args[2].equals("noprint"))
+                print = false;
 
-        System.out.println("Took time: " + (program.timeSolver(solver, s)/1000000000.0));
-        System.out.println("-----------------");
+        if(print) {
+            s.printBoard();
+            System.out.println("-----------------");
+
+            System.out.println("Took time: " + (program.timeSolver(solver, s)/1000000000.0));
+            System.out.println("-----------------");
+
+            s.printBoard();
+        } else {
+            System.out.println("solving " + args[0] + " using mode " + args[1] + " took time: " + (program.timeSolver(solver, s)/1000000000.0));
+        }
         
-        s.printBoard();
     }
 
     public long timeSolver(ISolver solver, Sudoku s) {
