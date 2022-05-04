98c0348630e120f4fc30e95b2555b43672fbd090
Saumitra Lohokare <saumitra.lohokare@gmail.com>
2022-05-04 01:47:34 -0400

    Better error messages

--- src_2/Program.java
+++ src_2/Program.java
@@ -1,15 +1,24 @@
 public class Program {
     
     public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("[USAGE]: ./run.sh <sudoku file> <0|1|2>");
+            System.exit(-1);
+        }
+
         Program program = new Program();
         Sudoku s = new Sudoku();
         s.readSudokyFromFile(args[0]);
-        
-        s.printBoard();
-        System.out.println("-----------------");
 
+        int choice = -1;
+        try {
+            choice = Integer.parseInt(args[1]); 
+        } catch (Exception e) {
+            System.err.println("[USAGE]: ./run.sh <sudoku file> <0|1|2>");
+            System.exit(-1);
+        }
         ISolver solver;
-        switch (Integer.parseInt(args[1])) {
+        switch (choice) {
             case 0: solver = new NaiveSolver();
             break;
             case 1: solver = new BTLoopsSolver();
@@ -18,6 +27,9 @@ public class Program {
             break;
             default: throw new IllegalArgumentException("Invalid solver.");
         }
+        
+        s.printBoard();
+        System.out.println("-----------------");
 
         System.out.println("Took time: " + (program.timeSolver(solver, s)/1000000000.0));
         System.out.println("-----------------");
