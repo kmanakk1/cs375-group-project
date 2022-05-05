d9cacca0ce07981028e89db8b4401946c3b3c62b
Kaesi Manakkal <kaesimanakkal@gmail.com>
2022-05-04 20:08:33 -0400

    cleanup

--- tests.sh
+++ tests.sh
@@ -4,10 +4,7 @@ PUZZLES="$(ls puzzles/|grep .txt)"
 testSudoku() {
     FILE=$1
     for i in 0 1 2; do
-        #echo "Running solver [${i}] against puzzle: ${FILE}"
         ./run.sh ${FILE} ${i} noprint
-        #echo -n 'Press enter'
-        #read dummy
     done
 }
 
